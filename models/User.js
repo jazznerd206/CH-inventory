fa2b8af5510c3d16e5bc95254ca07afcb65e28bc
Andrew Miller <206alm@gmail.com>
2020-03-03 05:02:33 -0800

    entire auth package removed, cleaned up user model and package.json files

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
 
 
 const User = new Schema({
@@ -8,7 +7,6 @@ const User = new Schema({
     email: String,
     password: String,
 });
- 
-User.plugin(passportLocalMongoose);
+
  
 module.exports = mongoose.model('User', User);

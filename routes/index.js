a2a5476564dd81d15ee1ba0bc45af0d1159d2246
Andrew Miller <206alm@gmail.com>
2020-03-03 15:44:36 -0800

    added nodemon to prod

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,7 @@ const barRoutes = require('./bar');
 const fritRoutes = require('./frit');
 const metalRoutes = require('./metals')
 const reportRoutes = require('./reports');
-<<<<<<< HEAD
 const authRoutes = require('./auth');
-=======
-//const authRoutes = require('./auth');
->>>>>>> 56ce7df3f9b7eea4dc3d5bbb3f55b191a1891cef
 
 
 // route director
@@ -16,11 +12,7 @@ router.use("/bar", barRoutes);
 router.use("/frit", fritRoutes);
 router.use('/metal', metalRoutes);
 router.use('/reports', reportRoutes);
-<<<<<<< HEAD
 router.use('/auth', authRoutes);
-=======
-//router.use('/auth', authRoutes);
->>>>>>> 56ce7df3f9b7eea4dc3d5bbb3f55b191a1891cef
 
 // load index handlebars
 router.get('/', function(req, res) {

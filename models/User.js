6e5a9c3b455b963d00770931607facd1f5e567ad
Andrew Miller <206alm@gmail.com>
2020-12-20 18:49:03 -0800

    change padding on mobile, small margin and padding

--- models/User.js
+++ models/User.js
@@ -4,8 +4,20 @@ const crypto = require('crypto');
 
 
 const User = new Schema({
-    username: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
-    email: {type: String, lowercase: true, unique: false, required: false, match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+    username: {
+        type: String, 
+        lowercase: true, 
+        required: [true, "can't be blank"], 
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid'], 
+        index: true
+    },
+    email: {
+        type: String, 
+        lowercase: true, 
+        unique: true, 
+        required: false, 
+        match: [/\S+@\S+\.\S+/, 'is invalid'], 
+        index: true},
     password: String,
 });
 
@@ -13,6 +25,5 @@ User.methods.setPassword = function(password){
     this.salt = crypto.randomBytes(16).toString('hex');
     this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
     };
-
- 
+    
 module.exports = mongoose.model('User', User);
